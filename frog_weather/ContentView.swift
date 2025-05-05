b3caf625b6f6337ffe44381a822a4203c08edf4e
Goathoon <ownest2@gmail.com>
2025-05-05 16:25:43 +0900

    Initial Commit

---
+++ frog_weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  frog_weather
+//
+//  Created by 김태훈 on 5/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ frog_weather/frog_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  frog_weatherApp.swift
+//  frog_weather
+//
+//  Created by 김태훈 on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct frog_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
